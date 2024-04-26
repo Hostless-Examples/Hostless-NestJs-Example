67c2d74be8b861e36e644fd4c26cdb1f52022d05
Adesoji Daniel <sojidaniels@gmail.com>
2024-04-26 12:34:26 +0100

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -11,5 +11,6 @@ async function bootstrap() {
   app.setViewEngine('ejs');
 
   await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0'); // this is important for Hostless
+
 }
 bootstrap();
